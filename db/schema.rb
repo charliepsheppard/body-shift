ebe81f4b2fe839ad9d2be47d25c62081daa4d423
Charlie Sheppard <sheppard.charlie24@gmail.com>
2019-03-01 09:11:07 -0500

    most recent updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_212144) do
+ActiveRecord::Schema.define(version: 2019_02_28_210909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_212144) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "session_token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
